4b1df0b0649faa7ae1ca6a766252fb71ff45a91f
CECIL7845 <odonkorcecil123@gmail.com>
2023-10-10 22:41:44 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let myCourses = ["learn CSS Animatons", "UI Design Fundamentals", "Intro to Clean Code"]
+
+function courses(arr) {
+    for (let i = 0; i < myCourses.length; i++){
+    console.log(arr[i])
+    }
+}
+
+courses(myCourses)
